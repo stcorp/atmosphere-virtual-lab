ad55f5acd2e78db0dd3c70a1ed052d5d9a3076e9
Mark Dufour <mark.dufour@stcorp.nl>
2022-05-18 10:33:07 +0200

    add ipywidgets as dep in setup.py
    ipyleaflet depends on it, but we are using it directly.. dunno.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description="Top-level package for an Atmosphere Virtual Lab installation",
     license="BSD",
     packages=["avl"],
-    install_requires=["requests", "numpy", "panel", "plotly", "pyproj", "vtk", "ipyleaflet", "scipy"],
+    install_requires=["requests", "numpy", "panel", "plotly", "pyproj", "vtk", "ipyleaflet", "ipywidgets", "scipy"],
     package_data={'': ['8k_earth_daymap.jpg']},
     python_requires='>=3.9',
 )
